988fefead48fc59182f9192d6b4d977d6b6cd392
CoralPink <teqt6ytqt@mozmail.com>
2023-04-08 15:34:05 +0900

    Make warnings easier to see.
    And while we're at it, organize the code😊

--- src/tailor.rs
+++ src/tailor.rs
@@ -9,33 +9,36 @@ use std::path::Path;
 
 lazy_static! {
     static ref TAILOR_RE: Regex =
-        Regex::new(r"(?m)^(\s*)!\[(?P<alt>[^]]*)]\((?P<path>[^)]*)\)$").unwrap();
+        Regex::new(r"(?m)^(\s*)!\[(?P<alt>[^]]*)]\((?P<url>[^)]*)\)$")
+            .unwrap();
 }
 
 pub fn measure(src: &str, mut book: Book) -> Result<Book, Error> {
     book.for_each_mut(|item| {
         if let BookItem::Chapter(chap) = item {
-            let dir = match Path::new(chap.path.as_ref().unwrap()).parent(){
-                Some(p) => p,
-                None => Path::new(""),
-            };
+            let mdfile = chap.path.as_ref().map_or("", |p| p.to_str().unwrap_or(""));
+            let dir = Path::new(mdfile).parent().unwrap_or_else(|| Path::new(""));
 
             chap.content = TAILOR_RE
                 .replace_all(&chap.content, |caps: &regex::Captures| {
-                    let path = caps.name("path").unwrap().as_str();
+                    let url = caps.name("url").unwrap().as_str();
+                    let path = Path::new(&src).join(dir.join(url));
 
-                    match open(Path::new(&src).join(dir.join(path))) {
+                    match open(&path) {
                         Ok(image) => {
                             format!(
                                 "<img src=\"{}\" alt=\"{}\" width=\"{}\" height=\"{}\" loading=\"lazy\">",
-                                path,
+                                url,
                                 caps.name("alt").unwrap().as_str(),
                                 image.width(),
                                 image.height(),
                             )
                         }
                         Err(_) => {
-                            eprintln!("Warning: Tailor could not find: {}/{}", dir.display(), path);
+                            eprintln!("\x1b[33m [Warning]\x1b[0m Tailor could not find: \x1b[35m{}\x1b[0m From \x1b[36m{}\x1b[0m",
+                                path.display(),
+                                mdfile
+                            );
                             chap.content.clone()
                         },
                     }
